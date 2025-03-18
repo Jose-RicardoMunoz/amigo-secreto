fcf7a4437e65698365f15f6b166faf7b804e87ec
José Ricardo Muñoz <equipolegionecompany@gmail.com>
2025-03-18 00:39:21 -0300

    Función agregarAmigo(): Captura y almacena nombres

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // Array para almacenar los nombres de los amigos
 let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let amigo = input.value.trim();
+
+    if (amigo !== "") {
+        alert("Por favor, inserte un nombre válido.");
+        return;
+    }
+
+    amigos.push(amigo);
+    input.value = ""; // Limpiar el campo de entrada
+
+    console.log(amigos); //Depuración
+}
